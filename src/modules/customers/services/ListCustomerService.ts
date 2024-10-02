3ee567fbea723371acb9603aaca8a53c60c4c9e0
alissonrhuant3 <alissonrhuant3@gmail.com>
2024-10-02 13:53:18 -0300

    Recurso de paginação

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -3,13 +3,24 @@ import AppError from '@shared/errors/AppError';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 import Customer from '../typeorm/entities/Customer';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as IPaginateCustomer;
   }
 }
 
