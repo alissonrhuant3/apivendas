0e6b470de38ffeafb0ea92cba38a03b903b405fa
alissonrhuant3 <alissonrhuant3@gmail.com>
2024-10-03 17:48:34 -0300

    Implementado Cache com o Redis

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 import AppError from '@shared/errors/AppError';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 interface IRequest {
   id: string;
@@ -25,6 +26,10 @@ class UpdateProductService {
     if (productExists && name !== product.name) {
       throw new AppError('There is already one product with this name');
     }
+
+     const redisCache = new RedisCache();
+
+     await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     product.name = name;
     product.price = price;
